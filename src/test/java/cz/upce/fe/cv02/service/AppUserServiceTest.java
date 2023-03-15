e34edc5229b961a090624e3ca85987cb1661c989
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-03-15 15:14:39 +0100

    change tests

--- src/test/java/cz/upce/fe/cv02/service/AppUserServiceTest.java
+++ src/test/java/cz/upce/fe/cv02/service/AppUserServiceTest.java
@@ -1,35 +1,33 @@
 package cz.upce.fe.cv02.service;
 
 import cz.upce.fe.cv02.Example;
-import cz.upce.fe.cv02.domain.AppUser;
 import cz.upce.fe.cv02.repository.AppUserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class AppUserServiceTest {
     @Autowired
     private AppUserService appUserService;
 
-    @MockBean
+    @Autowired
     private AppUserRepository appUserRepository;
 
 
     @BeforeEach
     void setUp() {
-        Mockito.when(appUserRepository.findById(100L))
-                .thenReturn(Optional.of(Example.EXISTTING));
+       appUserRepository.save(Example.EXISTTING);
 
     }
+
+    @AfterEach
+    void tearDown() {
+        appUserRepository.deleteAll();
+    }
+
     @Test
     void findById() throws ResourceNotFoundException {
         var expected = Example.EXISTTING;
